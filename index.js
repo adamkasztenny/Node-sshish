1b11f44f8d898d8c9ee0eeaa5498bcbd641299c0
slideon <adamkasztenny@gmail.com>
2016-04-11 19:10:36 -0400

    Add some toy example routes

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ var fs = require('fs');
 var mongoose = require('mongoose');
 var app = express();
 
-app.get('/server', function(req, res) {
+mongoose.connect('mongodb://localhost:27017/simple-server');
+
+var visitorSchema = mongoose.Schema({ name: String, age: Number});
+
+app.get('/source', function(req, res) {
     res.download('./index.js');
+});
+
+app.get('/', function(req, res) {
+    res.end('Welcome to my simple server');
+});
+
+// POST is preferable here
+app.get('/signup', function(req, res) {
+    var _vistor = new Visitor(req.query.name, req.query.age);
+    _vistor.save(function(err) {
+        if (err) {
+            console.debug(JSON.stringify(err));
+        }
+    });
 });
 
 console.log('Listening on localhost:5001');
