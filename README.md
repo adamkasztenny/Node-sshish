2ea7344bd12b78a7516d457b2bb6ca07c41ca7e5
slideon <adamkasztenny@gmail.com>
2016-04-11 20:00:09 -0400

    Add RESTful route for users to run commands & the server to log them

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Node SSH-ish Server
+A Node.js server that uses REST calls (/bash/:user/:command) to run a command on the server.
+
+I think it goes without saying that this server is NOT secure in any way.
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const exec = require('child_process').exec;
 
 mongoose.connect('mongodb://localhost:27017/simple-server');
 
-var visitorSchema = mongoose.Schema({name: String, age: Number});
+var visitorSchema = mongoose.Schema({user: {type: String, unique: true}, run: Array});
 var Visitor = mongoose.model('Visitor', visitorSchema);
 
 app.get('/source', function(req, res) {
@@ -16,15 +16,17 @@ app.get('/source', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-    res.end('Welcome to my simple server');
+    // TODO: replace w/ simple index.html
+    res.end('Welcome to my simple SSH-ish server. Navigate to /bash/:command to run a command.');
 });
 
 // POST is preferable here
 app.get('/signup', function(req, res) {
-    var _vistor = new Visitor({name: req.query.name, age: req.query.age});
+    var _vistor = new Visitor({user: req.query.user, run: []});
     _vistor.save(function(err) {
         if (err) {
             console.log(JSON.stringify(err));
+            return;
         }
     });
     console.log(JSON.stringify(_visitor));
@@ -32,14 +34,31 @@ app.get('/signup', function(req, res) {
 });
 
 // thanks to https://nodejs.org/api/child_process.html#child_process_child_process_execfile_file_args_options_callback
-app.get('/bash/:command', function (req, res) {
+app.get('/bash/:user/:command', function (req, res) {
     var command = req.params.command;
+    var user = req.params.user;
     var child = exec(command, (error, stdout, stderr) => {
         res.json(stdout);
         
         if (error) {
             console.log(error);
+            return;
         }
+
+        console.log(user + " ran " + command);
+
+        var result = {command: command,
+            date: new Date(),
+            stdout_result: stdout,
+            stderr_result: stderr
+        };
+        // thanks to http://stackoverflow.com/questions/7267102/how-do-i-update-upsert-a-document-in-mongoose
+        Visitor.findOneAndUpdate({user: user}, {$push: {run: result}}, {upsert:true}, function (err, doc) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+        });
     });
 });
 
