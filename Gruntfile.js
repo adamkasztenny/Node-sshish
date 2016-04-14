3325c8a46eebc0ff499acfdfcaf2116b8890b0f4
slideon <adamkasztenny@gmail.com>
2016-04-14 09:51:45 -0400

    Try to add node server task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,14 @@ module.exports = function(grunt) {
         }
       }
     },
-
+    run_node: { // thanks to https://github.com/jamesdbloom/grunt-run-node
+        start: {
+            options: {
+                stdio: [null, null, null ],
+            },
+            files: { src: [ 'index.js'] }
+        }
+    },
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
@@ -20,7 +27,8 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-run-node');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'run_node']);
 
 };
